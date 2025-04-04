a69158ec004b5c10f9f6435248ed41d2a461a6ea
Lmao Ded <121595359+TheLmaoDed@users.noreply.github.com>
2025-04-05 00:07:04 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// JavaScript to hide loading screen after the page fully loads
+
 window.addEventListener("load", () => {
     const loadingScreen = document.getElementById("loading-screen");
-    loadingScreen.style.display = "none";  // Hides loading screen after page loads
+    loadingScreen.style.display = "none";
 });
